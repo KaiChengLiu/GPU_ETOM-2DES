d9fb9f5652ec2107af5f550200f5cab0064f113f
KaiCheng Liu <andrew60511@gmail.com>
2025-02-25 12:50:48 +0000

    fix the cuda path and machine settings

--- script/Run_GPU_2DES.sh
+++ script/Run_GPU_2DES.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
-#PBS -N Run_GPU_2DES
-#PBS -r n
+#PBS -N GPU_2DES_job
+#PBS -l walltime=168:00:00
+#PBS -l mem=3GB,nodes=gpu02,ncpus=1
+#PBS -q GPU_queue
 #PBS -e Run_GPU_2DES.pbserr
 #PBS -o Run_GPU_2DES.pbslog
-#PBS -q long
-#PBS -l walltime=720:00:00,nodes=1:ppn=11
+
+
+
+source /usr/local/pbs-tools/pbs_prologue.sh
 
 # Load CUDA 11.8 environment
-module load cuda/11.8
-export CUDA_HOME=$HOME/cuda-11.8
+export CUDA_HOME=/usr/local/cuda-11.8
 export PATH=$CUDA_HOME/bin:$PATH
 export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
 
 # Define the GPU_2DES executable path
-GPU_2DES=$PBS_O_WORKDIR/GPU_2DES  # The executable is placed in the PBS working directory
+GPU_2DES="../src/GPU_2DES"  # The executable is placed in the PBS working directory
 
 # Change to the directory where the job was submitted
 cd $PBS_O_WORKDIR
@@ -55,3 +58,4 @@ wait
 
 # Log job completion time
 echo "=== GPU_2DES execution completed at $(date) ==="
+source /usr/local/pbs-tools/pbs_epilogue.sh
